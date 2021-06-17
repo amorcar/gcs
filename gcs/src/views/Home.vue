619f9878c24246589680d3e466511fa51cb8b791
Alejandro Morales <amorales@iuma.ulpgc.es>
2021-06-17 20:13:38 +0100

    add three columns grid layout with colors for easy development

--- gcs/src/views/Home.vue
+++ gcs/src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <p>Hola</p>
+  <v-row no-gutters>
+    <v-col>
+      <div class="d-block pa-2 blue white--text">
+        First
+      </div>
+    </v-col>
+    <v-col cols="6">
+      <div class="d-block pa-2 green white--text">
+        Second
+      </div>
+    </v-col>
+    <v-col>
+      <div class="d-block pa-2 yellow white--text">
+        Third
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
     },
   }
 </script>
