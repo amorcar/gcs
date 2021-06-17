34a4fdf3d896d4268eadac9bc6bbcf0c99e21538
Alejandro Morales <amorales@iuma.ulpgc.es>
2021-06-17 19:22:50 +0100

    add dynamic background color change for every status

--- gcs/src/components/SystemBar.vue
+++ gcs/src/components/SystemBar.vue
@@ -1,18 +1,53 @@
 <template>
-   <v-system-bar app>
+   <v-system-bar app
+    :lights-out="isLightsOut"
+    :color="backgroundColor"
+   >
       <v-spacer></v-spacer>
-      <v-icon>mdi-square</v-icon>
-      <v-icon>mdi-circle</v-icon>
-      <v-icon>mdi-triangle</v-icon>
+      <v-icon>mdi-wifi-strength-4</v-icon>
+      <v-icon>mdi-signal-cellular-outline</v-icon>
+      <v-icon>mdi-battery</v-icon>
+      <span>12:30</span>
     </v-system-bar> 
 </template>
 
 <script>
+
+const status = {
+  OK: 0,
+  WARNING: 1,
+  ERROR: 2,
+};
 export default {
   name: 'SystemBar',
+  data() {
+    return {
+      currentStatus: status.OK,
 
-  data: () => ({
-    //
-  }),
+    }
+  },
+  computed: {
+    backgroundColor() {
+      switch(this.currentStatus){
+        case status.OK:
+          return 'grey lighten-3';
+        case status.WARNING:
+          return 'warning';
+        case status.ERROR:
+          return 'error';
+        default:
+          return '';
+      }
+    },
+    isLightsOut() {
+      if (this.currentStatus == status.OK) {
+        return true
+      }
+      else {
+        return false
+      }
+    }
+
+  },
 };
 </script>
