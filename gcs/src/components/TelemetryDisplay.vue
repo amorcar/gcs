30694b5c29707dbc1a291543605009d2ae534c7e
Alejandro Morales <amorales@iuma.ulpgc.es>
2021-06-17 21:17:12 +0100

    add TelemetryDisplay component with a table data

---
+++ gcs/src/components/TelemetryDisplay.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-data-table
+    :headers="telemetryHeaders"
+    :items="telemetryItems"
+    hide-default-header
+    hide-default-footer
+  > </v-data-table>
+  <!-- <div class="d-block pa-2 blue white--text">First</div> -->
+</template>
+
+
+<script>
+export default {
+  name: "TelemetryDisplay",
+  data () {
+    return {
+      battery: {
+        voltage: 15,
+        remaining: 70,
+      },
+      gpsInfo: {
+        satellites: 2,
+        fixType: '2D Position'
+      },
+      telemetryHeaders: [
+          {
+            text: 'Topic',
+            align: 'start',
+            value: 'label',
+          },
+          { text: 'Data', value: 'value' },
+        ],
+      telemetryItems: [],
+    }
+  },
+  mounted() {
+    this.telemetryItems = [
+      { label: 'Voltage (V:', value: this.battery.voltage },
+      { label: 'Remaining (%):', value: this.battery.remaining },
+      { label: 'nº Satellites: ', value: this.gpsInfo.satellites },
+      { label: 'GPS Fix Type', value: this.gpsInfo.fixType },
+    ];
+  },
+  methods: {
+
+  },
+};
+</script>
--- gcs/src/views/Home.vue
+++ gcs/src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <v-row no-gutters>
     <v-col>
-      <div class="d-block pa-2 blue white--text">
-        First
-      </div>
+      <TelemetryDisplay/>
     </v-col>
     <v-col cols="6">
       <div class="d-block pa-2 green white--text">
@@ -19,11 +17,13 @@
 </template>
 
 <script>
+import TelemetryDisplay from '@/components/TelemetryDisplay'
 
   export default {
     name: 'Home',
 
     components: {
+      TelemetryDisplay,
     },
   }
 </script>
